63cf9c1e9774d22a1618df00e49c8820a1eaa6be
itangishaka1 <abdullahitangishaka@gmail.com>
2022-11-03 10:29:31 -0400

    Shopping Cart Design

---
+++ pages/Cart.jsx
@@ -0,0 +1,154 @@
+import styles from '../styles/Cart.module.css'
+import Image from 'next/image'
+
+const Cart = () => {
+  return (
+    <main className={styles.container}>
+      <div className={styles.left}>
+        <table className={styles.table}>
+          <tr className={styles.tr}>
+            <th>Product</th>
+            <th>Name</th>
+            <th>Extras</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Total</th>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.imgContainer}>
+                <Image
+                  src='/images/pizza.png'
+                  alt='pizza'
+                  fill
+                  style={{ objectFit: 'cover' }}
+                />
+              </div>
+            </td>
+            <td>
+              <span className={styles.name}>Pizza name</span>
+            </td>
+            <td>
+              <span className={styles.extras}>
+                Double ingredient, spicy sauce
+              </span>
+            </td>
+            <td>
+              <span className={styles.price}>$8.99</span>
+            </td>
+            <td>
+              <span className={styles.quantity}>2</span>
+            </td>
+            <td>
+              <span className={styles.total}>$38.99</span>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.imgContainer}>
+                <Image
+                  src='/images/pizza.png'
+                  alt='pizza'
+                  fill
+                  style={{ objectFit: 'cover' }}
+                />
+              </div>
+            </td>
+            <td>
+              <span className={styles.name}>Pizza name</span>
+            </td>
+            <td>
+              <span className={styles.extras}>
+                Double ingredient, spicy sauce
+              </span>
+            </td>
+            <td>
+              <span className={styles.price}>$8.99</span>
+            </td>
+            <td>
+              <span className={styles.quantity}>2</span>
+            </td>
+            <td>
+              <span className={styles.total}>$38.99</span>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.imgContainer}>
+                <Image
+                  src='/images/pizza.png'
+                  alt='pizza'
+                  fill
+                  style={{ objectFit: 'cover' }}
+                />
+              </div>
+            </td>
+            <td>
+              <span className={styles.name}>Pizza name</span>
+            </td>
+            <td>
+              <span className={styles.extras}>
+                Double ingredient, spicy sauce
+              </span>
+            </td>
+            <td>
+              <span className={styles.price}>$8.99</span>
+            </td>
+            <td>
+              <span className={styles.quantity}>2</span>
+            </td>
+            <td>
+              <span className={styles.total}>$38.99</span>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.imgContainer}>
+                <Image
+                  src='/images/pizza.png'
+                  alt='pizza'
+                  fill
+                  style={{ objectFit: 'cover' }}
+                />
+              </div>
+            </td>
+            <td>
+              <span className={styles.name}>Pizza name</span>
+            </td>
+            <td>
+              <span className={styles.extras}>
+                Double ingredient, spicy sauce
+              </span>
+            </td>
+            <td>
+              <span className={styles.price}>$8.99</span>
+            </td>
+            <td>
+              <span className={styles.quantity}>2</span>
+            </td>
+            <td>
+              <span className={styles.total}>$38.99</span>
+            </td>
+          </tr>
+        </table>
+      </div>
+      <div className={styles.right}>
+        <div className={styles.wrapper}>
+          <h2 className={styles.title}>CART TOTAL</h2>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Subtotal:</b>$79.60
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Discount:</b>$0.00
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Total:</b>$79.60
+          </div>
+          <button className={styles.btn}>CHECKOUT NOW!</button>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default Cart
